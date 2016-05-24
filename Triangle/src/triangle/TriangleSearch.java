98528f436aebba84b06954640409ee3c5a3da11e
jdoop <jrh_nju@sina.com>
2016-05-24 22:10:25 +0800

    add combiner

--- Triangle/src/triangle/TriangleSearch.java
+++ Triangle/src/triangle/TriangleSearch.java
@@ -42,15 +42,43 @@ public class TriangleSearch {
 		}
 	}
 
-	public static class TriangleSearchReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-		private static Text current_num = new Text("num");
-		
+	public static class TriangleSearchCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private static IntWritable result = new IntWritable();
+		private static IntWritable zero = new IntWritable(0);
+
 		public void reduce(Text key, Iterable<IntWritable> values, Context context)
 				throws IOException, InterruptedException {
 			boolean sign = false;
-			int sum = 0;
+			int sum = 0, val = 0;
 			for (IntWritable value : values) {
-				int val = value.get();
+				val = value.get();
+				if (val == 0) {
+					sign = true;
+				} else {
+					sum += val;
+				}
+			}
+			if (sign) {
+				context.write(key, zero);
+			}
+			if (sum != 0) {
+				result.set(sum);
+				context.write(key, result);
+			}
+		}
+	}
+
+	public static class TriangleSearchReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private static Text current_num = new Text("num");
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			boolean sign = false;
+			int sum = 0, val = 0;
+			for (IntWritable value : values) {
+				val = value.get();
 				if (val == 0) {
 					sign = true;
 				} else {
@@ -59,11 +87,17 @@ public class TriangleSearch {
 			}
 			if (sign && sum != 0) {
 				Configuration conf = context.getConfiguration();
-				int num = conf.getInt("triangle",0) + sum;
+				int num = conf.getInt("triangle", 0) + sum;
 				conf.setInt("triangle", num);
-				context.write(key, new IntWritable(sum));
-				context.write(current_num, new IntWritable(num));
+				// context.write(key, new IntWritable(sum));
+				// context.write(current_num, new IntWritable(num));
 			}
+		}
+
+		public void cleanup(Context context) throws IOException, InterruptedException {
+			Configuration conf = context.getConfiguration();
+			int num = conf.getInt("triangle", 0);
+			context.write(current_num, new IntWritable(num));
 		}
 	}
 
@@ -76,6 +110,7 @@ public class TriangleSearch {
 		job1.setOutputValueClass(IntWritable.class);
 		job1.setMapperClass(TriangleSearchMapper.class);
 		job1.setReducerClass(TriangleSearchReducer.class);
+		job1.setCombinerClass(TriangleSearchCombiner.class);
 		FileInputFormat.addInputPath(job1, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job1, new Path(args[1]));
 		job1.waitForCompletion(true);
